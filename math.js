351762296d7b17c5aa6160780ae9e27430a342f0
Nazril <73063248+NightHawkOne@users.noreply.github.com>
2024-06-22 15:29:50 +0700

    Update Math Javascript.js

--- math.js
+++ math.js
@@ -1,1 +1,41 @@
-console.log("c")
+class Math{
+  
+  constructor(){
+        
+    this.pilihan = prompt("Pilih operator: ")
+    this.proses() 
+  }
+  proses(){
+    this.angka1 = parseInt(prompt("Angka Satu"))
+    this.angka2 = parseInt(prompt("Angka Dua"))
+    
+    if(this.pilihan === "+"){this.tambah(this.angka1,this.angka2)}
+    else if(this.pilihan === "-"){this.kurang(this.angka1,this.angka2)}
+    else if(this.pilihan === "/"){this.bagi(this.angka1,this.angka2)}
+    else if(this.pilihan === "*"){this.kali(this.angka1,this.angka2)}
+    else if(this.pilihan === "%"){this.modulus(this.angka1,this.angka2)};
+  };
+  //this.tambah = (angka1,angka2) => {console.log(angka1 + angka2)})
+  tambah(angka1,angka2){
+    let hasil = angka1 + angka2
+    console.log(hasil)
+  };
+  kurang(angka1,angka2){
+    let hasil = angka1 - angka2
+    console.log(hasil)
+  };
+  bagi(angka1,angka2){
+    let hasil = angka1 / angka2
+    console.log(hasil)
+  };
+  kali(angka1,angka2){
+    let hasil = angka1 * angka2
+    console.log(hasil)
+  };
+  modulus(angka1,angka2){
+    let hasil = angka1 % angka2
+    console.log(hasiil)
+  };
+}
+
+new Math()
